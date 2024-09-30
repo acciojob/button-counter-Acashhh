02b7eb692f56e8ea5ba7423cfbfdb798da37065e
Acashhh <96347766+Acashhh@users.noreply.github.com>
2024-09-30 20:36:08 +0000

    right update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import './../styles/App.css';
+
+const App = ()=>{
+    const [count, setCount]=useState(10);
+
+    const inc = ()=>{
+        setCount(count+1);
+    };
+     
+    return(
+        <div>
+            <p>Button clicked {count} times</p>
+            <button onClick={inc}>
+                Click me
+            </button>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const App=()=>{
-    const [count, setCount]=useState(0);
-    
-    const inc=()=>{
-        setCount(count+1);
-    }
-    return(
-        <div>
-            <p>Button clicked {count} times</p>
-            <button onClick={inc}>Click me</button>
-        </div>
-    )
-}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
